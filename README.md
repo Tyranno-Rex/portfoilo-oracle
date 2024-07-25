065f11620c90000501967fbb452c78f685f2789b
정은성 <80687043+Tyranno-Rex@users.noreply.github.com>
2024-07-25 14:36:47 +0900

    init portfolio oracle server

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # portfoilo-oracle
 this is portfolio-server in oracle
+
+## notion url
+https://www.notion.so/portfolio-project-b0c9080783014e5ea696da2ed9ac6972?pvs=4
+
+
+## Project Crawling
+
+PROJECT_NAME : portfolio-oracle
+PROJECT_DESCRIPTION : This repository showcases Jeong Eun Seong's portfolio. It contains various projects that I have worked on. The website presents these projects in a 3D space. The project leverages several advanced technologies, including fine-tuned AI models using OpenAI's GPT-3.5, Three.js for 3D rendering, and an optimal algorithm to categorize and display the projects. It utilizes full-stack development for both backend and frontend functionalities. Additionally, the server crawls my GitHub repository daily to update the portfolio website automatically. 
+PROJECT_URL : 'https://github.com/Tyranno-Rex/portfolio-oracle'
+PROJECT_COMPLETION_STATUS : FALSE
+PROJECT_MULTI : FALSE
+PROJECT_SUBPROJECT : NONE
+PROJECT_CATEGORY : 'graphic', 'ai', 'web/mobile', 'algorithm', 'fullstack'
---
+++ springboot/src/main/java/site/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,46 @@
+package site.springboot.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(
+                        (csrfConfig) -> csrfConfig.disable()
+                )
+                .headers(
+                        (headerConfig) -> headerConfig.frameOptions(
+                                frameOptionsConfig -> frameOptionsConfig.disable()
+                        )
+                )
+                .authorizeHttpRequests((authorizeRequest) -> authorizeRequest
+                        .requestMatchers("/", "/css/**", "images/**", "/js/**", "/login/**", "/logout/**", "/posts/**", "/comments/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout( // 로그아웃 성공 시 / 주소로 이동
+                        (logoutConfig) -> logoutConfig.logoutSuccessUrl("/")
+                );
+        http
+                .oauth2Login(
+                        (oauth) ->
+                                oauth.userInfoEndpoint(
+                                        (endpoint) -> endpoint.userService(customOAuth2UserService)
+                                )
+                );
+
+
+        return http.build();
+    }
+}
