107495b70bd46d48014d6cbeadd3e477f35b4bff
정은성 <80687043+Tyranno-Rex@users.noreply.github.com>
2024-07-29 13:12:13 +0900

    authencation api update

--- springboot/src/main/java/site/springboot/Question/QuestionController.java
+++ springboot/src/main/java/site/springboot/Question/QuestionController.java
@@ -9,17 +9,15 @@ import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/question")
+@RequestMapping("/api/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
     @PostMapping("/send")
     public String receviedQuestion(@RequestBody QuestionDTO questionDTO, Principal principal) {
-
         Question question = new Question();
         boolean Flag_saveReceivedQuestion = questionService.saveReceviedQuestion(questionDTO, principal);
-
         if (Flag_saveReceivedQuestion) {
             return "success";
         } else {
